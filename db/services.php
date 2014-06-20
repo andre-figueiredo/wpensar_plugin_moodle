aab5399ac12413d9f25aef289d202c66e9c7d27c
Matheus Guilarducci <mguilarducci@gmail.com>
2014-06-19 22:55:19 -0300

    role and enrol

--- db/services.php
+++ db/services.php
@@ -13,13 +13,20 @@ $functions = array(
         'methodname' => 'get_users_by_field',
         'classpath' => 'user/externallib.php',
         'description' => 'pega usuarios',
-        'type' => 'write'
+        'type' => 'read'
     ),
     'local_wpensar_update_user' => array(
         'classname' => 'core_user_external',
         'methodname' => 'update_users',
         'classpath' => 'user/externallib.php',
         'description' => 'Atualiza',
+        'type' => 'write'
+    ),
+    'local_wpensar_enrol_user' => array(
+        'classname' => 'local_wpensar_external',
+        'methodname' => 'wpensar_enrol_user',
+        'classpath' => 'local/wpensar_plugin_moodle/externallib.php',
+        'description' => 'Enrol user',
         'type' => 'write'
     ),
 );
--- externallib.php
+++ externallib.php
@@ -3,32 +3,63 @@
 require_once($CFG->libdir . "/externallib.php");
 
 class local_wpensar_external extends external_api {
-    /*public static function wpensar_insert_user_parameters() {
+    public static function wpensar_enrol_user_parameters() {
         return new external_function_parameters(
             array(
-                'user' => new external_single_structure(
-                    array(
-                        'firstname' => new external_value(PARAM_TEXT, 'First name'),
-                        'lastname' => new external_value(PARAM_TEXT, 'Last name'),
-                        'email' => new external_value(PARAM_TEXT, 'Email'),
-                        'username' => new external_value(PARAM_TEXT, 'Username'),
-                        'password' => new external_value(PARAM_TEXT, 'Password'),
-                        'confirmed' => new external_value(PARAM_INT, 'Confirmed'),
-                        'mnethostid' => new external_value(PARAM_INT, 'x'),
-                        'lang' => new external_value(PARAM_TEXT, 'Language')
+                'enrols' => return new external_multiple_structure(
+                    new external_single_structure(
+                        array(
+                            'userid' => new external_value(PARAM_INT, 'userid'),
+                            'courseid' => new external_value(PARAM_INT, 'course id'),
+                            'roleid' => new external_value(PARAM_INT, 'role id'),
+                        )
                     )
                 )
             )
         );
     }
 
-    public static function wpensar_insert_user($user) {
+    public static function wpensar_enrol_user($enrols) {
         global $DB;
-        $params = self::validate_parameters(self::wpensar_insert_user_parameters(),
-                array('user' => $user));
+
+        $params = self::validate_parameters(self::wpensar_enrol_user_parameters(), array('enrols' => $enrols));
+        $transaction = $DB->start_delegated_transaction();
+        foreach ($params['enrols'] as $enrol) {
+            $userid = $enrol['userid'];
+            $courseid = $enrol['courseid'];
+            $roleid = $enrol['roleid'];
+
+            $context = $DB->get_record('context', array('instanceid' => $courseid, 'contextlevel' => 50));
+            if (empty($context->id)) {
+                throw new invalid_parameter_exception('Invalid context');
+            }
+
+            $enrol = $DB->get_record('context', array('enrol' => 'manual', 'courseid' => $courseid));
+            if (empty($enrol->id)) {
+                throw new invalid_parameter_exception('Invalid enrol');
+            }
+
+            $role_assignments = new stdClass();
+            $role_assignments->roleid = $roleid;
+            $role_assignments->contextid = $context->id;
+            $role_assignments->userid = $userid;
+            $DB->insert_record('role_assignments', $role_assignments);
+
+            $user_enrolments = new stdClass();
+            $user_enrolments->status = 0;
+            $user_enrolments->enrolid = $enrol->id;
+            $user_enrolments->userid = $userid;
+            $user_enrolments->timestart = date('Y-m-d');
+            $user_enrolments->timend = 0;
+            $user_enrolments->timecreated = date('Y-m-d'); 
+            $user_enrolments->timemodified = 0; 
+            $DB->insert_record('user_enrolments', $user_enrolments);
+        }
+        $transaction->allow_commit();
+        return null;
     }
 
-    public static function wpensar_insert_user_returns() {
-        return new external_value(PARAM_INT, 'The moodle user id');
-    }*/
+    public static function wpensar_enrol_user_returns() {
+        return null;
+    }
 }
